ff916781b50ca580b72b23ab897494ea00e30eef
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-20 13:52:37 +0000

    CompletedThreeTasks
    made changes to the code that so that 2 variables and useState. then used if statements in order to create feedback for an inputed answer.
    
    Tasks:
    / Create a useState called answer, set it to 5
    // Create a useState called message. This will sit above the input to display a message to the user
    // Create a function called makeGuess, update the message to reflect whether the guess was too high, too low or correct. Invoke it when the button is clicked.

--- week6day1/src/guessGame.js
+++ week6day1/src/guessGame.js
@@ -1,26 +1,37 @@
-import { useState } from 'react';
 import './guessGame.css';
+import { useState } from 'react';
 
 function RandomNumberGame()
 {
 
     const [guess, setGuess] = useState("")
+    const [answer, setAnswer] = useState("5")
+    const [message, setMessage] = useState ("Guess a number between 1 and 5")
+
+// Create a useState called answer, set it to 5
+// Create a useState called message. This will sit above the input to display a message to the user 
+// Create a function called makeGuess, update the message to reflect whether the guess was too high, too low or correct. Invoke it when the button is clicked.
+
+    function MakeGuess(){
+        if (guess < 5){
+            setMessage("guess was too low")
+        } else if (guess > 5){
+            setMessage("guess was too high")
+        }else{
+            setMessage("guess correct!!!")
+        }
+    }
+
 
     return(
         <div className='guess-container'>
-            <h1 className='question'>
-            Guess a number!
-            </h1>
-
-            {guess}
-            
-            <input placeholder='Place your guess here...' type="text" className='guess-input' onChange={(event) => {setGuess(event.target.value)}}/>
-           
-            
-            <h1><button className='guess-button'>GUESS</button></h1>
+            <h1 className='question'>Guess a number!</h1>
+            {/* {typeof guess} */}
+            <h2 className='message' >{message}</h2>
+            <input placeholder='Place your guess here...' type="number" className='guess-input' onChange={(event) => {setGuess(parseInt(event.target.value))}}/>
+            <h1><button className='guess-button' onClick={MakeGuess} >GUESS</button></h1>
         </div>        
     )
 }
 
 export default RandomNumberGame;
-
