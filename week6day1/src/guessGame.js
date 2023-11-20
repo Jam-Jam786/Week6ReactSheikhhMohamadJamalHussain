a6564d088dd0a2ed8ce483b1e22c8db90e7a7f0c
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-20 14:24:51 +0000

    NumberOfAttempts(3)
    increased the size of the box in the guessGame.css.
    
    Then created a variable for attempts with the value of 3. then added code to if statement in order for the decrease of 1 if the wrong answer is guessed for either too high or too low of a guess but also a null value if guessed correct.

--- week6day1/src/guessGame.js
+++ week6day1/src/guessGame.js
@@ -7,18 +7,24 @@ function RandomNumberGame()
     const [guess, setGuess] = useState("")
     const [answer, setAnswer] = useState("5")
     const [message, setMessage] = useState ("Guess a number between 1 and 5")
+    const [attempts, setattempts] = useState (3)
 
 // Create a useState called answer, set it to 5
 // Create a useState called message. This will sit above the input to display a message to the user 
 // Create a function called makeGuess, update the message to reflect whether the guess was too high, too low or correct. Invoke it when the button is clicked.
 
     function MakeGuess(){
-        if (guess < 5){
+        if (guess < 5) {
             setMessage("guess was too low")
-        } else if (guess > 5){
+            setattempts(attempts - 1)
+        } 
+        else if (guess > 5) {
             setMessage("guess was too high")
-        }else{
+            setattempts(attempts - 1)
+        }
+        else {
             setMessage("guess correct!!!")
+            setattempts(attempts == 1)
         }
     }
 
@@ -29,7 +35,8 @@ function RandomNumberGame()
             {/* {typeof guess} */}
             <h2 className='message' >{message}</h2>
             <input placeholder='Place your guess here...' type="number" className='guess-input' onChange={(event) => {setGuess(parseInt(event.target.value))}}/>
-            <h1><button className='guess-button' onClick={MakeGuess} >GUESS</button></h1>
+            <h1>number of attempts {attempts}</h1>
+            <h1><button className='guess-button' onClick={MakeGuess}>GUESS</button></h1>
         </div>        
     )
 }
