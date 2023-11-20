266799dd215db5a195804a828786741cadbd52ab
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-20 16:10:49 +0000

    conditionalRendering
    Adding the conditional rendering variable gameOver with the value being false. so if the else statement is fired then the the game conditional render is met and output a game over message with other information.

--- week6day1/src/guessGame.js
+++ week6day1/src/guessGame.js
@@ -1,6 +1,9 @@
 import './guessGame.css';
 import { useState } from 'react';
 
+
+
+
 function RandomNumberGame()
 {
 
@@ -8,24 +11,41 @@ function RandomNumberGame()
     const [answer, setAnswer] = useState("5")
     const [message, setMessage] = useState ("Guess a number between 1 and 5")
     const [attempts, setattempts] = useState (3)
+    const [gameOver, setGameOver] = useState(false)
+    const [endmessage, setEndmessage] = useState("")
+
 
 // Create a useState called answer, set it to 5
 // Create a useState called message. This will sit above the input to display a message to the user 
 // Create a function called makeGuess, update the message to reflect whether the guess was too high, too low or correct. Invoke it when the button is clicked.
 
     function MakeGuess(){
-        if (guess < 5) {
-            setMessage("guess was too low")
-            setattempts(attempts - 1)
-        } 
-        else if (guess > 5) {
-            setMessage("guess was too high")
-            setattempts(attempts - 1)
+        if(attempts > 1){
+            if (guess < 5) {
+                setMessage("guess was too low")
+                setattempts(attempts - 1)
+            } 
+            else if (guess > 5) {
+                setMessage("guess was too high")
+                setattempts(attempts - 1)
+            }
+            else {
+                setMessage("guess correct!!!")
+                setattempts(attempts == 1)
+            }
+        }else{
+            setGameOver(true)
+            setMessage("game over")
+            setEndmessage("the correct answer was")
+
+
         }
-        else {
-            setMessage("guess correct!!!")
-            setattempts(attempts == 1)
         }
+
+    function Reset(){
+        setattempts(3)
+        setGameOver(false)
+        setMessage("Guess a number between 1 and 5")
     }
 
 
@@ -34,9 +54,18 @@ function RandomNumberGame()
             <h1 className='question'>Guess a number!</h1>
             {/* {typeof guess} */}
             <h2 className='message' >{message}</h2>
-            <input placeholder='Place your guess here...' type="number" className='guess-input' onChange={(event) => {setGuess(parseInt(event.target.value))}}/>
+            
             <h1>number of attempts {attempts}</h1>
-            <h1><button className='guess-button' onClick={MakeGuess}>GUESS</button></h1>
+            {gameOver == false&&
+            <>
+            <input placeholder='Place your guess here...' type="number" className='guess-input' onChange={(event) => {setGuess(parseInt(event.target.value))}}/>
+            <button className='guess-button' onClick={MakeGuess}>GUESS</button>
+            </>
+            }
+            {gameOver == true&&
+            <h4>{endmessage} {answer}</h4>
+            }
+            <button className='reset-button' onClick={Reset} >RESET</button>
         </div>        
     )
 }
