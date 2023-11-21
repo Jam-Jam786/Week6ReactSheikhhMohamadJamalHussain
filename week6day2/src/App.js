1787324187df959f38b0965d869e3414674cfc7e
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-21 12:26:51 +0000

    ExportedAndImported
    applied function import and export in order to connect the CSS page to the JS for SocialMediaSearchPage then connected it to the App.js page.

--- week6day2/src/App.js
+++ week6day2/src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+
+import './SocialMediaSearchPage'
 import './App.css';
+import UsersData from './SocialMediaSearchPage'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <UsersData></UsersData>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ week6day2/src/SocialMediaSearchPage.js
@@ -0,0 +1,25 @@
+import './SocialMediaSearchPage.css';
+// import { useState } from 'react';
+
+function UsersData()
+{
+
+    return(
+        <div className='searchPage-container'>
+            <div className='profilePic'>
+
+            </div>
+            <h1 className='name'> hello world  </h1>
+            <h1 className='age'>    </h1>
+            <h1 className='proffesional'>   </h1>
+        </div>
+
+    )
+    
+}
+
+
+
+
+
+export default UsersData;
