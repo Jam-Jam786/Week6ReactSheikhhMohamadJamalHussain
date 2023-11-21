ce16b44c953f4ddb6a999a341837d64d714fcd68
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-21 14:03:30 +0000

    addedImageAndAddedCSS
    added the image tag and then src in order to add the link to the image. then added width and height tag in order to make change to image because it doesn't apply with CSS changes to it. added display:flex and then changed the flex direction: row in order to put the photo on the left and the text on the right.

--- week6day2/src/SocialMediaSearchPage.js
+++ week6day2/src/SocialMediaSearchPage.js
@@ -3,15 +3,19 @@ import './SocialMediaSearchPage.css';
 
 function UsersData()
 {
-
     return(
-        <div className='searchPage-container'>
-            <div className='profilePic'>
 
+        <div className='searchPage-container'>
+
+            <div className='profilePic'>
+                <img width={"50vw"} height={"100vh"} src="https://i.dailymail.co.uk/i/pix/2011/10/21/article-2052035-0E767BDF00000578-546_233x423.jpg" alt="cannot load"/>
             </div>
-            <h1 className='name' >jamal</h1>
-            <h1 className='age'>27</h1>
-            <h1 className='proffesional'>student</h1>
+
+            <div className='information'>
+                <h1 className='name' >jamal</h1>
+                <h1 className='age'>27</h1>
+                <h1 className='proffesion'>student</h1>
+            </div>
         </div>
 
     )
