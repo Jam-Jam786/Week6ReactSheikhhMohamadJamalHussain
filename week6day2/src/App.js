46b621571da73c3a8c6744e4d78ac80e477e9ffd
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-21 16:00:39 +0000

    created props
    created data for the props in the App.js and then made copied of the function 3 times. Then applied the props in the socialMediaSearchPage.js in the const variable so they applied to the page.

--- week6day2/src/App.js
+++ week6day2/src/App.js
@@ -2,16 +2,36 @@
 import './SocialMediaSearchPage'
 import './App.css';
 import UsersData from './SocialMediaSearchPage'
+import { useState } from 'react';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <UsersData></UsersData>
-      </header>
-    </div>
-  );
+
+  const [userDetails, setuserDetails] = useState([])
+
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          {userDetails.map(() => {
+            return(
+              <div>
+                
+              </div>
+            )
+
+
+          })}
+          
+            return(
+              <UsersData name = {"Mickey Mouse"} age = {"94"} prof = {"charactor"} url = {"https://c8.alamy.com/comp/CRWNYH/mickey-mouse-CRWNYH.jpg"} ></UsersData>
+              <UsersData name = {"Donald Duck"} age = {"89"} prof = {"charactor"} url = {"https://qph.cf2.quoracdn.net/main-qimg-d883cfd4405aad021805789fab5c3694-pjlq"} ></UsersData>
+              <UsersData name = {"Goofy"} age = {"91"} prof = {"charactor"} url = {"https://www.disneyclips.com/images/images/goofy28.png"} ></UsersData>
+
+        </header>
+      </div>
+    );
 }
 
 
--- week6day2/src/SocialMediaSearchPage.js
+++ week6day2/src/SocialMediaSearchPage.js
@@ -1,20 +1,19 @@
 import './SocialMediaSearchPage.css';
 import { useState } from 'react';
 
-function UsersData(){
+function UsersData(props){
 
-const [UserName, setUserName] = useState("jamal")
-const [UserAge, setUserAge] = useState("27")
-const [Userprofession, setUserProfession] = useState("profession")
-
-
+const [UserName, setUserName] = useState(props.name)
+const [UserAge, setUserAge] = useState(props.age)
+const [Userprofession, setUserProfession] = useState(props.prof)
+const [UserPhoto, setUserPhoto] = useState(props.url)
 
     return(
 
         <div className='searchPage-container'>
 
             <div className='profilePic'>
-                <img width={"50vw"} height={"100vh"} src="https://i.dailymail.co.uk/i/pix/2011/10/21/article-2052035-0E767BDF00000578-546_233x423.jpg" alt="cannot load"/>
+                <img width={"50vw"} height={"100vh"} src={UserPhoto} alt="cannot load"/>
             </div>
 
             <div className='information'>
