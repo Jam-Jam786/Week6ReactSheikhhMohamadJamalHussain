83248d1ffff6f00806606e6f335bd0649056bde2
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-22 10:48:38 +0000

    created Friend Request Button
    created UseState thats I've placed in the Button text location then created a function to pass into the on click function so that the text in the button changes when someone clicks on it.

--- week6day2/src/SocialMediaSearchPage.js
+++ week6day2/src/SocialMediaSearchPage.js
@@ -1,19 +1,29 @@
 import './SocialMediaSearchPage.css';
 import { useState } from 'react';
 
+
 function UsersData(props){
 
 const [UserName, setUserName] = useState(props.name)
 const [UserAge, setUserAge] = useState(props.age)
 const [Userprofession, setUserProfession] = useState(props.prof)
 const [UserPhoto, setUserPhoto] = useState(props.url)
+const [FriendButton, setFriendButton] = useState("Friend Request")
+
+
+function ButtonMessageSent(){
+    setFriendButton ("Request sent")
+}
 
     return(
 
-        <div className='searchPage-container'>
+        <div className='searchPage-container' >
 
             <div className='profilePic'>
                 <img width={"50vw"} height={"100vh"} src={UserPhoto} alt="cannot load"/>
+                <button className = 'friendButton' onClick={(event) => (ButtonMessageSent())}>{FriendButton}</button>
+
+
             </div>
 
             <div className='information'>
