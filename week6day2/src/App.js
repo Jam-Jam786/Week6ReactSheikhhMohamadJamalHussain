92fd7d2490042f53cfcafd4331c0b9188e43fcc8
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-22 09:29:16 +0000

    added array and passed the data using the map function
    using an array in the variable const UserDetails then using the map function in order to pass the dat into the code to appear on the webpage

--- week6day2/src/App.js
+++ week6day2/src/App.js
@@ -7,31 +7,52 @@ import { useState } from 'react';
 
 function App() {
 
-  const [userDetails, setuserDetails] = useState([])
+  const [userDetails, setuserDetails] = useState([
+    {
+      name : "Mickey Mouse",
+      age : "94",
+      prof : "charactor",
+      url : "https://c8.alamy.com/comp/CRWNYH/mickey-mouse-CRWNYH.jpg"
+    },
+    {
+      name : "Donald Duck",
+      age : "89",
+      prof : "charactor",
+      url : "https://qph.cf2.quoracdn.net/main-qimg-d883cfd4405aad021805789fab5c3694-pjlq"
+    },
+    {
+      name : "Goofy",
+      age : "91",
+      prof : "charactor",
+      url : "https://www.disneyclips.com/images/images/goofy28.png"
+    }
 
+  ])
+
+  
   
     return (
       <div className="App">
         <header className="App-header">
 
-          {userDetails.map(() => {
+          {userDetails.map((dataBase) => {
             return(
               <div>
+                <UsersData name = {dataBase.name} age = {dataBase.age} prof = {dataBase.prof} url = {dataBase.url}/>
                 
               </div>
             )
 
-
           })}
           
-            return(
+            {/* return(
               <UsersData name = {"Mickey Mouse"} age = {"94"} prof = {"charactor"} url = {"https://c8.alamy.com/comp/CRWNYH/mickey-mouse-CRWNYH.jpg"} ></UsersData>
               <UsersData name = {"Donald Duck"} age = {"89"} prof = {"charactor"} url = {"https://qph.cf2.quoracdn.net/main-qimg-d883cfd4405aad021805789fab5c3694-pjlq"} ></UsersData>
               <UsersData name = {"Goofy"} age = {"91"} prof = {"charactor"} url = {"https://www.disneyclips.com/images/images/goofy28.png"} ></UsersData>
-
+            ); */}
         </header>
       </div>
-    );
+    )         
 }
 
 
