0553df9df7f35673fc5ed37b5ecd4741f9119fcd
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-23 14:38:38 +0000

    added the (=) and (clear) buttons

--- functioning/src/Calculator.js
+++ functioning/src/Calculator.js
@@ -6,34 +6,52 @@ import './Calculator.css'
 {
 
 const [Numbers, setNumbers] = useState(["9", "8", "7", "6", "5", "4", "3", "2", "1", "0" ])
+const [Operators, setOperators] = useState ([ "%","X","-","+" ])
+
+// create a use state for the operators, just like you did with the numbers
+// use a map function to render the operator buttons in the operator div
 
     return(
-        <div className='calculatar-container'>
+        < div className='calculatar-container'>
 
             <div className="calculator-top">
-
-                 <div className='calculator-display'>
-                    
-                 </div>
-
+                 <div className='calculator-display'></div>
             </div>
 
-            <div className='calculator-operators'>
-            {/* division, times, minus and addition */}
-                <div className = 'division-button' style={{width:"25%" , height:"100%"}}>%</div>
+            <div className='calculator-operators'>  {Operators.map((Operator) => {
+                return(
+                        <div className='operator-btn' style = {{width:"25%" , height:"100%", backgroundColor: "orange"}}>
+                            {Operator}
+                        </div>
+                );
+                })}
+            </div>   
+                
+                
+                {/* <div className = 'division-button' style={{width:"25%" , height:"100%"}}>%</div>
                 <div className = 'times-button' style={{width:"25%" , height:"100%"}}>X</div>
                 <div className = 'minus-button' style={{width:"25%" , height:"100%"}}>-</div>
-                <div className='addition-button' style={{width:"25%" , height:"100%"}}>+</div>
-            </div>
+                <div className='addition-button' style={{width:"25%" , height:"100%"}}>+</div> */}
+            
 
             <div className='calculator-numbers'>  {Numbers.map((number) => {
                 return(
-                    <div className='calc-btn' style={{width: "33.3%", height: "25%"}}>
+                    <div className='calc-btn' style={{width: "31%", height: "23%", backgroundColor: "red"}} >
                         {number}
                     </div>
                 );
                 })}
-                </div>
+            <div className='calc-btn' style={{width: "31%", height: "23%"}} >
+                        clear
+            </div>
+            <div className='calc-btn' style={{width: "31%", height: "23%"}} >
+                        =
+            </div> 
+
+            </div>
+
+
+
 
         </div>
      )   
