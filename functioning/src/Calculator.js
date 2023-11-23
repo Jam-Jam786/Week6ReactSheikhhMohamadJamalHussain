83ccaf139fed0b32ddf6c0311f5b41a18d373585
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-23 16:10:31 +0000

    added an onclick function to the "clear" and  "=" and output to the calculator-display

--- functioning/src/Calculator.js
+++ functioning/src/Calculator.js
@@ -6,21 +6,26 @@ import './Calculator.css'
 {
 
 const [Numbers, setNumbers] = useState(["9", "8", "7", "6", "5", "4", "3", "2", "1", "0" ])
-const [Operators, setOperators] = useState ([ "%","X","-","+" ])
 
 // create a use state for the operators, just like you did with the numbers
 // use a map function to render the operator buttons in the operator div
+const [Operators, setOperators] = useState ([ "%","*","-","+" ])
+const [Output, setOutput] = useState("")
+
+
 
     return(
         < div className='calculatar-container'>
 
             <div className="calculator-top">
-                 <div className='calculator-display'></div>
+                 <div className='calculator-display'>
+                    {Output}
+                 </div>
             </div>
 
             <div className='calculator-operators'>  {Operators.map((Operator) => {
                 return(
-                        <div className='operator-btn' style = {{width:"25%" , height:"100%", backgroundColor: "orange"}}>
+                        <div className='operator-btn' style = {{width:"25%" , height:"100%", backgroundColor: "orange"}} onClick={() => {setOutput(Output + Operator)}}>
                             {Operator}
                         </div>
                 );
@@ -36,15 +41,18 @@ const [Operators, setOperators] = useState ([ "%","X","-","+" ])
 
             <div className='calculator-numbers'>  {Numbers.map((number) => {
                 return(
-                    <div className='calc-btn' style={{width: "31%", height: "23%", backgroundColor: "red"}} >
-                        {number}
-                    </div>
+                    // add an onclick parameter that will add the buttons number to the end of the output
+                        <div className='calc-btn' style={{width: "31%", height: "23%", backgroundColor: "red"}} onClick={() => {setOutput(Output + number)}}>
+                            {number}
+                        </div>
                 );
                 })}
-            <div className='calc-btn' style={{width: "31%", height: "23%"}} >
+            <div className='calc-btn' style={{width: "31%", height: "23%"}} onClick={() => setOutput("")}>
                         clear
             </div>
-            <div className='calc-btn' style={{width: "31%", height: "23%"}} >
+            
+            {/* evaulated this string as a function and it is not recommended to use this in professional settings */}
+            <div className='calc-btn' style={{width: "31%", height: "23%"}} onClick={() => {setOutput(Function("return " + Output))}}>
                         =
             </div> 
 
