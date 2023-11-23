e23bd577fa14d8f0ff1eafe28f48f6122b95c6b5
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-23 11:58:33 +0000

    applied the CSS through style tag  on the div

--- functioning/src/Calculator.js
+++ functioning/src/Calculator.js
@@ -8,13 +8,23 @@ import './Calculator.css'
 
             <div className="calculator-top">
 
-                 <div className='calculator-display'></div>
+                 <div className='calculator-display'>
+                    
+                 </div>
 
             </div>
 
-            <div className='calculator-operators'></div>
+            <div className='calculator-operators'>
+            {/* division, times, minus and addition */}
+                <div className = 'division-button' style={{width:"25%" , height:"100%"}}>%</div>
+                <div className = 'times-button' style={{width:"25%" , height:"100%"}}>X</div>
+                <div className = 'minus-button' style={{width:"25%" , height:"100%"}}>-</div>
+                <div className='addition-button' style={{width:"25%" , height:"100%"}}>+</div>
+            </div>
 
-            <div className='calculator-numbers'></div>
+            <div className='calculator-numbers'>
+
+            </div>
 
         </div>
      )   
